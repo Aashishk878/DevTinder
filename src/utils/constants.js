be06dc4568d61b3965471b62eed9e22c9b24d9a0
AASHISHK4568 <aashishk4568@gmail.com>
2025-04-14 17:02:25 +0530

    dynamic base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = 
+    location.hostname == "localhost" ? "http://localhost:7777" : "https://devtinder-backend-qwl6.onrender.com";
